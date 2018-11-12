487a74f6f2604a11b13fe249a6b33215e60ce50c
iZezoh <Mr.Ziyad-2009@hotmail.com>
2018-11-12 21:01:15 +0300

    Initial Commit

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Ziyad Alamri on 12/11/2018.
+//  Copyright © 2018 Ziyad Alamri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
